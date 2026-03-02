0e54073a44a791a2d0dc0a2161a0d3f0313959e8
Jonathon Alder <jonathon.alder@gmail.com>
2026-03-02 17:23:26 -0500

    add .gitignore with lazy-lock

--- lua/config/lualine-cfg.lua
+++ lua/config/lualine-cfg.lua
@@ -34,7 +34,12 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = {
+      {
+        'filename',
+        path = 4
+      }
+    },
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
