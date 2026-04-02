a020e26accceed5939c1dafed10fbddc44e5bb42
Jonathon Alder <jonathon.alder@mercury.com>
2026-04-02 11:48:16 -0400

    remove branch from lualine

--- lua/config/lualine-cfg.lua
+++ lua/config/lualine-cfg.lua
@@ -33,7 +33,8 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    -- lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {'diff', 'diagnostics'},
     lualine_c = {
       {
         'filename',
