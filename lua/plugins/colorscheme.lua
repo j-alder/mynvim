77c227339452152f6b920fd80ae99b8b50d7dc6b
Jonathon Alder <jonathon.alder@gmail.com>
2026-06-25 20:54:00 -0400

    clean themes; add variants

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 return {
-  {"catppuccin/nvim", lazy = true, name = "catppuccin", priority = 1000},
-  {"ellisonleao/gruvbox.nvim", lazy = true, name = "gruvbox", priority = 1000, config = true},
-  { "ntk148v/habamax.nvim", lazy = true, name = "habamax", priority = 1000, dependencies={ "rktjmp/lush.nvim" } },
+  { "catppuccin/nvim", lazy = true, priority = 1000 },
+  { "ellisonleao/gruvbox.nvim", lazy = true, priority = 1000, config = true },
+  { "EdenEast/nightfox.nvim", lazy = true, priority = 1000 },
 }
 
--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -3,18 +3,15 @@ return {
   lazy = false,
   config = function()
     require("themery").setup({
-      themes = {{
-	      name = "Gruvbox",
-	      colorscheme = "gruvbox",
-      },
-      {
-	      name = "Catppuccin",
-	      colorscheme = "catppuccin",
-      },
-      {
-	      name = "Habamax",
-	      colorscheme = "habamax",
-      }}
+      themes = {
+        { name = "catppuccin latte", colorscheme = "catppuccin-latte" },
+        { name = "catppuccin frappe", colorscheme = "catppuccin-frappe" },
+        { name = "catppuccin mocha", colorscheme = "catppuccin-mocha" },
+        { name = "dayfox", colorscheme = "dayfox" },
+        { name = "dawnfox", colorscheme = "dawnfox" },
+        { name = "duskfox", colorscheme = "duskfox" },
+        { name = "nightfox", colorscheme = "nightfox" },
+      }
     })
   end
 }
